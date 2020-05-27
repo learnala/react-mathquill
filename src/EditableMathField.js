b2df825829aa42bb68b42e12cc5d5d852c51c75a
bernardobgam <40729003+bernardobgam@users.noreply.github.com>
2020-05-27 13:35:02 +1000

    removed examples and recreated components using hooks

--- src/EditableMathField.js
+++ src/EditableMathField.js
@@ -2,27 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { MathQuill } from './mathquill-loader'
 
-class EditableMathField extends React.Component {
-  constructor(props) {
-    super(props)
+const EditableMathField = props => {
+  const { latex, onChange, config, mathquillDidMount, ...otherProps } = props
+  let element = null
+  let mathField = null
+  let ignoreEditEvents = 4
 
-    this.element = null
-    this.mathField = null
-
-    // MathJax apparently fire 4 edit events on startup.
-    this.ignoreEditEvents = 4
-  }
-
-  componentDidMount() {
+  React.useEffect(() => {
     let config = {
       restrictMismatchedBrackets: true,
       handlers: {},
     }
 
-    if (this.props.config) {
+    if (props.config) {
       config = {
         ...config,
-        ...this.props.config,
+        ...props.config,
       }
     }
 
@@ -33,52 +28,50 @@ class EditableMathField extends React.Component {
         configEditHandler()
       }
 
-      if (this.ignoreEditEvents > 0) {
-        this.ignoreEditEvents -= 1
+      if (ignoreEditEvents > 0) {
+        ignoreEditEvents -= 1
         return
       }
-      if (this.props.onChange) {
-        this.props.onChange(mathField)
+      if (onChange) {
+        onChange(mathField)
       }
     }
 
-    this.mathField = MathQuill.MathField(this.element, config)
-    this.mathField.latex(this.props.latex || '')
+    mathField = MathQuill.MathField(element, config)
+    mathField.latex(latex || '')
 
-    if (this.props.mathquillDidMount) {
-      this.props.mathquillDidMount(this.mathField)
+    if (mathquillDidMount) {
+      mathquillDidMount(mathField)
     }
-  }
+  },[])
 
   /**
    * Update the mathfield's latex value if the value passed as `props.latex` doesn't match
    */
-  componentDidUpdate(prevProps) {
-    const { mathField } = this
-    const { latex } = this.props
-    if (mathField && prevProps.latex !== latex && mathField.latex() !== latex) {
-      mathField.latex(latex)
-    }
-  }
 
-  render() {
-    const {
-      latex,
-      onChange,
-      config,
-      mathquillDidMount,
-      ...otherProps
-    } = this.props
+   React.useEffect(() => {
+     if (mathField && mathField.latex() !== latex) {
+       mathField.latex(latex)
+     }
+   },[mathField, latex])
+  // componentDidUpdate(prevProps) {
+  //   const { mathField } = this
+  //   const { latex } = this.props
+  //   if (mathField && prevProps.latex !== latex && mathField.latex() !== latex) {
+  //     mathField.latex(latex)
+  //   }
+  // }
 
-    return (
-      <span
-        {...otherProps}
-        ref={x => {
-          this.element = x
-        }}
-      />
-    )
-  }
+
+
+  return (
+    <span
+      {...otherProps}
+      ref={x => {
+        this.element = x
+      }}
+    />
+  )
 }
 
 EditableMathField.propTypes = {
--- src/StaticMathField.js
+++ src/StaticMathField.js
@@ -2,39 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { MathQuill } from './mathquill-loader'
 
-class StaticMathField extends React.Component {
-  constructor(props) {
-    super(props)
+const StaticMathField = (props) => {
+  const { mathquillDidMount, children, ...otherProps } = props
+  let element = null
+  let mathField = null
 
-    this.element = null
-    this.mathField = null
+  const updateMathquill = () => {
+    mathField = MathQuill.StaticMath(element)
 
-    this.updateMathquill.bind(this)
-  }
-
-  updateMathquill() {
-    this.mathField = MathQuill.StaticMath(this.element)
-
-    if (this.props.mathquillDidMount) {
-      this.props.mathquillDidMount(this.mathField)
+    if (mathquillDidMount) {
+      mathquillDidMount(mathField)
     }
   }
 
-  render() {
-    const { mathquillDidMount, children, ...otherProps } = this.props
-
-    return (
-      <div
-        {...otherProps}
-        ref={x => {
-          this.element = x
-          this.updateMathquill()
-        }}
-      >
-        {children}
-      </div>
-    )
-  }
+  return (
+    <span
+      {...otherProps}
+      ref={x => {
+        this.element = x
+        this.updateMathquill()
+      }}
+    >
+      {children}
+    </span>
+  )
 }
 
 StaticMathField.propTypes = {
